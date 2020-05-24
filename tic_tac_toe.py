630f8748f3104902b0873d8cdb688aeabd31b7bc
Alisha Rafiq Metkari <63468707+alishametkari@users.noreply.github.com>
2020-05-24 22:32:49 +0530

    Add files via upload

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -6,10 +6,8 @@ def board( lis ) :
         else :
             print( lis[i], end = " | " )
 def Input( lis, i, value ) :
-    #function to input index of box
     lis[i] = value
 def getindexes( lis, value ) :
-    #function to get indexes of o and x occupied boxes
     index = []
     indexpos = 0
     while True :
@@ -21,13 +19,11 @@ def getindexes( lis, value ) :
             break
     return index
 def abnormal_end( lis ) :
-    #to check game ends normally or not
     if lis.count( 'o' ) + lis.count( 'x' ) == 9 :
         return 1
     else :
         return 0
 def check1( lis,win ) :
-    #to check whethe player 1 wins the game
     for i in range( len( win ) ) :
         count1 = 0
         for j in range( len( win[i] ) ) :
@@ -36,7 +32,6 @@ def check1( lis,win ) :
         if count1 == 3 :
             return 1
 def check2( lis, win ) :
-    #to check whether player 2 wins the game
     for i in range( len( win ) ) :
         count2 = 0
         for j in range( len( win[i] ) ) :
@@ -47,14 +42,10 @@ def check2( lis, win ) :
 lis = [ '', '', '', '', '', '', '', '', '' ]           #empty board for game
 board( lis )
 print( "\n" )
-#list containing winning positions with indexes
 win = [ [ 0, 3, 6 ], [ 1, 4, 7 ], [ 2, 5, 8 ], [ 0, 1, 2 ], [ 3, 4, 5 ], [ 6, 7, 8 ], [ 0, 4, 8 ], [ 2, 4, 6 ] ]
-#dictionary with value o and x and players as a key
 dic = { 'play1' : 'o', 'play2' : 'x' }
-#information of players
 player1 = str( input( "enter name of player 1 : " ) )
 player2 = str( input( "enter name of player 2 : " ) )
-#actual game
 while True :
         while True :
             try :
